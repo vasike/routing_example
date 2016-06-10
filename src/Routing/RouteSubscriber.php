8bf3d752a5236d61e5e871de2a89da157a9a9aca
tavi toporjinschi <vasike@gmail.com>
2016-06-10 17:02:03 +0000

    # Dynamic routes example.

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -9,12 +9,36 @@ namespace Drupal\routing_example\Routing;
 
 
 use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 /**
  * Listens to the dynamic route events.
  */
 class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function routes() {
+    $routes = array();
+    // Declares a single route under the name 'routing_example.dynamic_route'.
+    // Returns an array of Route objects.
+    $routes['routing_example.dynamic_route'] = new Route(
+    // Path to attach this route to:
+      '/routing_example/dynamic-route',
+      // Route defaults:
+      array(
+        '_controller' => '\Drupal\routing_example\Controller\RoutingExampleController::base',
+        '_title' => 'Routing Example: Dynamic routes'
+      ),
+      // Route requirements:
+      array(
+        '_permission'  => 'access content',
+      )
+    );
+    return $routes;
+  }
 
   /**
    * {@inheritdoc}
